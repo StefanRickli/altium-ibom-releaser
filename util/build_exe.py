0bee4b7eba689b7da9818b541618bd6581219f7a
Stefan Rickli <git@stefanrickli.dev>
2025-04-11 13:37:53 +0200

    chore: use release folder for binary location

--- util/build_exe.py
+++ util/build_exe.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import shutil
 import subprocess
 
 def build():
@@ -11,7 +12,12 @@ def build():
         "-n", "altium_ibom_releaser",
         "src/altium_ibom_releaser/__main__.py"
     ], shell=True)
-    print("Wrote executable to dist/altium_ibom_releaser.exe")
+
+    shutil.copyfile(
+        Path("dist/altium_ibom_releaser.exe"),
+        Path("release/altium_ibom_releaser.exe"),
+    )
+    print("Copied executable to release folder.")
 
 if __name__ == "__main__":
     build()
